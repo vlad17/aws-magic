ce47e5b71892837e9247b90094d7a0e497b99479
Vladimir Feinberg <vladf@berkeley.edu>
2017-07-15 12:39:18 -0700

    g3 instance added

---
+++ oregon-ubuntu16.04-g3.4xlarge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Configure a g3.4xlarge instance with the default deep learning AMI
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: oregon-ubuntu16.04-p2.xlarge.sh name"
+    exit 1
+fi
+
+# get the correct ami
+region=$(aws configure get region)
+if [ $region != "us-west-2" ]; then
+    echo "us-west-2 (oregon) region only"
+    exit 1
+fi
+
+ami="ami-835b4efa"
+instanceType="g3.4xlarge"
+
+$(dirname $(readlink -f "$0"))/setup-instance.sh "$ami" "$instanceType" "$1"
